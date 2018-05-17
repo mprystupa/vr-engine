32fc5402ccdd074b7c7146ed93e1449c79b05fb0
YoYoshua <MarcinP32@gmail.com>
2018-05-17 18:53:12 +0200

    Add basic scripts

---
+++ Assets/move.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+	public float spread = 1.25F;
+	public float smoothTime = 0.3F;
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 target;
+	private Vector3 startPosition;
+	private bool flag = false;
+	private Vector3 center;
+
+	// Use this for initialization
+	void Start ()
+	{
+		center = transform.GetComponent<Renderer>().bounds.center;
+		target = new Vector3(spread * center.x, spread * center.y, spread * center.z);
+		Debug.Log(transform.GetComponent<Renderer>().bounds.center);
+		Debug.Log(target);
+		startPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown("a"))
+		{
+			flag = !flag;
+		}
+
+		if (flag == true)
+		{
+			transform.localPosition = Vector3.SmoothDamp(transform.localPosition, target, ref velocity, smoothTime);	
+		}
+		else if (flag == false)
+		{
+			transform.localPosition = Vector3.SmoothDamp(transform.localPosition, startPosition, ref velocity, smoothTime);	
+		}
+	}
+}
---
+++ Assets/parentScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Internal;
+using UnityEngine;
+
+public class parentScript : MonoBehaviour
+{
+
+	public Transform transform;
+	private float i = 0.0F;
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform)
+		{
+			Debug.Log("Child:", child);
+			child.gameObject.AddComponent<move>();
+			i += 0.25F;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
